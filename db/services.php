96966668142f3763c6f5bfcfd9bc3e7723127ed7
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2016-08-17 15:05:06 +1000

    Add services file

---
+++ db/services.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Web services for auth_userkey.
+ *
+ * @package    auth_userkey
+ * @copyright  2016 Dmitrii Metelkin (dmitriim@catalyst-au.net)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$functions = array(
+    'auth_userkey_request_login_url' => array(
+        'classname'   => 'auth_userkey_external',
+        'methodname'  => 'request_login_url',
+        'classpath'   => 'auth/userkey/externallib.php',
+        'description' => 'Return one time key based login URL',
+        'type'        => 'read',
+    )
+);
+
+$services = array(
+    'Request login URL Service' => array(
+        'functions' => array ('auth_userkey_request_login_url'),
+        'restrictedusers' => 0,
+        'enabled' => 1,
+    )
+);
