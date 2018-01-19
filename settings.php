19486a79b0c8ea4f40559e5885205c4b4f616d1d
hoz1982 <hoz1982@users.noreply.github.com>
2018-01-19 12:06:40 +0100

    Settings.php

--- settings.php
+++ settings.php
@@ -1,6 +1,5 @@
 <?php
 // This file is part of Moodle - http://moodle.org/
-//
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation,  either version 3 of the License,  or
