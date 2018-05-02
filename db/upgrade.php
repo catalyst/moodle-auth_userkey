15cfe700413a7f933fc84a8faf38e65d556b168b
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2018-05-02 10:30:07 +1000

    Fix broken SQL

--- db/upgrade.php
+++ db/upgrade.php
@@ -29,7 +29,7 @@ function xmldb_auth_userkey_upgrade($oldversion) {
 
     if ($oldversion < 2018050200) {
         // Confirm all previously created users.
-        $DB->execute('UPDATE {user} SET confirmed=1 WHERE auth="userkey"');
+        $DB->execute("UPDATE {user} SET confirmed=? WHERE auth=?", array(1, 'userkey'));
         upgrade_plugin_savepoint(true, 2018050200, 'auth', 'saml2');
     }
 
